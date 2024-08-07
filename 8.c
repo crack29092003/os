0ff50e7df5d5705e526b4389d33df05a99a1da4b
crack29092003 <164165412+crack29092003@users.noreply.github.com>
2024-08-08 02:04:50 +0530

    Update 8.c

--- 8.c
+++ 8.c
@@ -1,42 +1,6 @@
-//Write a program to simulate ls command using system calls
-#include <stdio.h>
-#include <sys/types.h>
-#include <dirent.h>
+//8(i)ls command----------------------------------------------
+
 #include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-    DIR *dp;
-    struct dirent *dirp;
-
-    if (argc < 2) {
-        printf("\nYou are not passing the directory\n");
-        exit(1);
-    }
-
-    if ((dp = opendir(argv[1])) == NULL) {
-        printf("\nCannot open directory %s\n", argv[1]);
-        exit(1);
-    }
-
-    while ((dirp = readdir(dp)) != NULL) {
-        printf("%s\n", dirp->d_name);
-    }
-
-    closedir(dp);
-    return 0;
-}
-
-/*Write a program to generate and print Fibonacci series with the
-following requirements:
- - Parent program should create a child and distribute the task of
-generating Fibonacci no to its child.
- - The code for generating Fibonacci series should reside in different
-program.
- - Child should write the generated Fibonacci series to a shared memory.
- - Parent process has to print by retrieving the Fibonacci series from the
-shared memory.
-i) Implement the above using shm_open and mmap*/
-//parent.c
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
@@ -45,58 +9,68 @@ i) Implement the above using shm_open and mmap*/
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
+#include <sys/mman.h>
+int main(int argc,char *argv[])
+{
     int i;
     pid_t pid;
+    int k;
+    int n1,n2,n3;
     const int SIZE = 4096;
-    int shmid;
+        int shm_fd;
     void *ptr;
-
-    if (argc > 1) {
+    if (argc > 1)
+    {
         sscanf(argv[1], "%d", &i);
-        if (i < 1) {
+        if (i < 1)
+        {
             printf("Error input: %d\n", i);
             return 0;
         }
-    } else {
-        printf("Error: Not passing N in command line\n");
-        return 1;
     }
-
-    shmid = shmget((key_t) 1122, SIZE, 0666 | IPC_CREAT);
-    if (shmid == -1) {
-        perror("shmget");
-        return 1;
+    else
+    {
+      printf("Error: not passing N in command line\n");
+      exit(0);
     }
-
-    ptr = shmat(shmid, NULL, 0);
-    if (ptr == (void *) -1) {
-        perror("shmat");
-        return 1;
-    }
-
     pid = fork();
-    if (pid == 0) {
-        execlp("./child", "child", argv[1], NULL);
-    } else if (pid > 0) {
+    if (pid == 0)// CHILD
+    {   
+        execlp("./fib","fib",argv[1],NULL);
+    }
+    else if (pid > 0)
+    {
         wait(NULL);
         printf("\nPARENT: child completed\n");
-        printf("Parent printing:\n");
-        printf("%s\n", (char *)ptr);
-        shmdt(ptr);
-        shmctl(shmid, IPC_RMID, NULL);
-    } else {
-        perror("fork");
-        return 1;
-    }
 
+        shm_fd = shm_open("VSS", O_RDONLY, 0666);
+        ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+        printf("Parent printing:\n");
+        printf("%s ", (char *)ptr);
+        shm_unlink("VSS");
+    }
     return 0;
 }
 
+//execute -------------:-
+//mkdir SW
+//cd SW
+//gedit -s p1.c
+//gedit -s p2.sh
+//ls
+//p1.c   p2.sh
+//cd ..
+//gedit -s lab4.c
+//gcc lab4.c
+//chmod 777 lab4.c
+//gcc lab4.c
+// ./a.out SW
 
-//child.c
+
+//8(ii)----------------------------fibonacci
+//using shm_OPEN and mmap
+//parent.c
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
@@ -105,50 +79,98 @@ int main(int argc, char *argv[]) {
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-    int k = 2, n1, n2, n3;
-    void *ptr;
+#include <sys/mman.h>
+int main(int argc,char *argv[])
+{
+    int i;
+    pid_t pid;
+    int k;
+    int n1,n2,n3;
     const int SIZE = 4096;
-
-    int shmid = shmget((key_t) 1122, SIZE, 0666);
-    if (shmid == -1) {
-        perror("shmget");
-        return 1;
+        int shm_fd;
+    void *ptr;
+    if (argc > 1)
+    {
+        sscanf(argv[1], "%d", &i);
+        if (i < 1)
+        {
+            printf("Error input: %d\n", i);
+            return 0;
+        }
     }
-
-    ptr = shmat(shmid, NULL, 0);
-    if (ptr == (void *) -1) {
-        perror("shmat");
-        return 1;
+    else
+    {
+      printf("Error: not passing N in command line\n");
+      exit(0);
     }
-
-    printf("CHILD:\n");
-    int i = atoi(argv[1]);
-    n1 = 0;
-    n2 = 1;
-    sprintf(ptr, "%d ", n1);
-    ptr += strlen(ptr);
-    printf("%d ", n1);
-    sprintf(ptr, "%d ", n2);
-    ptr += strlen(ptr);
-    printf("%d ", n2);
-
-    while (k < i) {
-        n3 = n1 + n2;
-        sprintf(ptr, "%d ", n3);
-        printf("%d ", n3);
-        n1 = n2;
-        n2 = n3;
-        ptr += strlen(ptr);
-        k++;
+    pid = fork();
+    if (pid == 0)// CHILD
+    {   
+        execlp("./fib","fib",argv[1],NULL);
     }
+    else if (pid > 0)
+    {
+        wait(NULL);
+        printf("\nPARENT: child completed\n");
 
-    printf("\n");
-
-    shmdt(ptr);
-
+        shm_fd = shm_open("VSS", O_RDONLY, 0666);
+        ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+        printf("Parent printing:\n");
+        printf("%s ", (char *)ptr);
+        shm_unlink("VSS");
+    }
     return 0;
 }
+
+//child.c
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include<stdlib.h>
+int main(int argc,char *argv[])
+{
+         int k=2,n1,n2,n3;
+         void*ptr;
+         int shm_fd = shm_open("VSS", O_CREAT | O_RDWR, 0666);
+         ftruncate(shm_fd,4096);
+         ptr = mmap(0,4096,PROT_WRITE, MAP_SHARED, shm_fd, 0);
+         printf("CHILD:\n");
+         int i=atoi(argv[1]);
+         n1=0;
+         n2=1;
+         sprintf(ptr,"%d ",n1);
+         ptr+=strlen(ptr);
+         printf("%d ",n1);
+         sprintf(ptr,"%d ",n2);
+         ptr+=strlen(ptr);
+         printf("%d ",n2);
+         while (k!=i)
+         {
+                n3=n1+n2;
+                sprintf(ptr,"%d ", n3);
+                printf("%d ", n3);
+                n1=n2;
+                n2=n3;
+                ptr += strlen(ptr);
+                k++;
+	}
+}
+	
+
+Output:
+$ gcc parent.c -lrt
+$ gcc child.c -o fib -lrt
+$ ./a.out 5
+CHILD:
+0 1 1 2 3
+PARENT: child completed
+Parent printing:
+0 1 1 2 3
 
