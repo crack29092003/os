6823bd74ba106bb42576e70cfae6bce88ac0a924
crack29092003 <164165412+crack29092003@users.noreply.github.com>
2024-08-13 21:39:57 +0530

    Update 8.c

--- 8.c
+++ 8.c
@@ -1,57 +1,27 @@
-//8(i)ls command----------------------------------------------
-
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-int main(int argc,char *argv[])
-{
-    int i;
-    pid_t pid;
-    int k;
-    int n1,n2,n3;
-    const int SIZE = 4096;
-        int shm_fd;
-    void *ptr;
-    if (argc > 1)
-    {
-        sscanf(argv[1], "%d", &i);
-        if (i < 1)
-        {
-            printf("Error input: %d\n", i);
-            return 0;
-        }
-    }
-    else
-    {
-      printf("Error: not passing N in command line\n");
-      exit(0);
-    }
-    pid = fork();
-    if (pid == 0)// CHILD
-    {   
-        execlp("./fib","fib",argv[1],NULL);
-    }
-    else if (pid > 0)
-    {
-        wait(NULL);
-        printf("\nPARENT: child completed\n");
-
-        shm_fd = shm_open("VSS", O_RDONLY, 0666);
-        ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
-        printf("Parent printing:\n");
-        printf("%s ", (char *)ptr);
-        shm_unlink("VSS");
-    }
-    return 0;
+// Simulation of "ls" command 
+ 
+  #include<stdio.h> 
+  #include<sys/types.h> 
+  #include<dirent.h>  
+#include<stdlib.h> 
+void  main(int argc, char *argv[]) 
+{ 
+DIR *dp; 
+struct dirent *dirp; 
+if(argc<2) 
+{ 
+printf("\n You are not passing the directory\n"); 
+exit(1); 
+} 
+if((dp=opendir(argv[1]))==NULL) 
+{ 
+printf("\nCannot open it does't exist %s file!\n",argv[1]);  
+exit(1); 
+} 
+while((dirp=readdir(dp))!=NULL) 
+printf("%s\n",dirp->d_name); 
+closedir(dp); 
 }
-
 //execute -------------:-
 //mkdir SW
 //cd SW
