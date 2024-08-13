ee6e2bcb741d94a73336d3ccea6ad5d75ca36bcc
crack29092003 <164165412+crack29092003@users.noreply.github.com>
2024-08-13 21:16:02 +0530

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,86 +1,49 @@
-//11(a)---main thread------
-
-//parent.c
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <stdlib.h>
-int main (int arguementCount, char *arguementVector[]) {
-int i, k, n1, n2, sharedMemoryFileDescriptor, j;
-const int SIZE = 4096;
-pid_t processID;
-void *ptr;
-if (arguementCount > 2) {
-sscanf(arguementVector[1], "%d", &i);
-sscanf(arguementVector[2], "%d", &j);
-if (i < 2) {
-printf("Error input: %d\n", i);
-return 0;
-}
-} else {
-printf("Error: Minimum of two arguements must be passed as command line
-arguements!");
-exit(0);
-}
-processID = fork();
-if (processID == 0) {
-execlp("./child.c", "prime", arguementVector[1], arguementVector[2], NULL);
-} else if (processID > 0) {
-wait(NULL);
-printf("\nParent: child complete!\n");
-sharedMemoryFileDescriptor = shm_open("VSS", O_RDONLY, 0666);
-ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED,
-sharedMemoryFileDescriptor, 0);
-printf("%s", (char *) ptr);
-shm_unlink("VSS");
-}
-return 0;
-}
-
-//child.c
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
+//program with two threads and main thread; also passing parameters to child threads
+#include<stdio.h>
 #include<stdlib.h>
-#include <math.h>
-int main(int arguementCount,char *arguementVector[]) {
-void *ptr;
-int sharedMemoryFileDescriptor = shm_open("VSS", O_CREAT | O_RDWR, 0666);
-ftruncate(sharedMemoryFileDescriptor,4096);
-ptr = mmap(0,4096,PROT_WRITE, MAP_SHARED, sharedMemoryFileDescriptor, 0);
-printf("CHILD:\n");
-int i = atoi(arguementVector[1]);
-int j = atoi(arguementVector[2]);
-int flag = 0;
-printf("The prime numbers in the range %d and %d are:\n", i, j);
-for (int num = i; num < j; num++) {
-flag = 0;
-for (int k = 2; k <= n/2; k++) {
-if (num % k == 0) {
-flag = 1;
-break;
+#include<unistd.h>
+#include<pthread.h>
+void *sum(void *arg);
+void *mul(void *arg);
+int sm=0, prod=1;
+void main(int argc, char * argv[])
+{
+  pthread_t T1,T2;
+  pthread_attr_t attr;//set of thread attributes
+  pthread_attr_init(&attr);//get the default attributes
+    
+  pthread_create(&T1, &attr, sum, argv[1]);
+  pthread_create(&T2, &attr, mul, argv[1]);
+  pthread_join(T1,NULL);
+  pthread_join(T2,NULL);
+  
+  printf("Inside main thread\n");
+  printf("sum=%d\n",sm);
+  printf("product=%d\n",prod);
+ }
+void *sum(void *parm) 
+{ 
+  int i, n; 
+   n = atoi(parm);//string to integer i.e., ASCII to int
+  printf("inside  sum thread\n");
+  
+  for(i=1; i<=n;i++)
+  {
+    sm+=i;     
+  }
+ printf("sum thread completed\n");
 }
+void *mul(void *parm)
+{
+  int i, n;
+  n = atoi(parm);
+  printf("inside mul thread\n");
+  for(i=2; i<=n;i++)
+  {
+    prod =prod *i;
+  }
+  printf("mul thread completed  product\n");
 }
-if (flag == 0) {
-printf("%d\t", num);
-}
-}
-printf("\n");
-}
-
 
 //11(b)----------producer-consumer using mutex and semaphores-----
 
